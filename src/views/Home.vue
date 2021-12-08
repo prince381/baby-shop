7c1cea3d6bd62d91bd8f25385b891630990f185f
Prince Owusu <powusu381@gmail.com>
2021-12-08 14:43:43 +0100

    finished the hero section of the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="home container">
-    <i class="fas fa-user"></i> This is a user icon.
+    <section class="hero">
+      <div class="hero-content">
+        <div class="bg-img">
+          <img src="../assets/images/hero_img.jpg" alt="hero image">
+        </div>
+        <div class="text-content">
+          <h1 class="main-text">
+            <span>DISCOVER &</span>
+            <span>SHOP THE TREND</span>
+          </h1>
+          <p class="subtext">Discover and shop all trending and quality clothes, shoes, toys and everthing you need for your kids.</p>
+          <button class="btn btn-hero">Shop Now</button>
+        </div>
+        <div class="bottom-info">
+          <h1 class="info">275 Shops</h1>
+          <h1 class="info">64318 People Served</h1>
+          <h1 class="info">64318 Registered Users</h1>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
